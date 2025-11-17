8dd2b61dc5453e66f17c0e352ce0dec6868c61f7
jpcamppp <jp.campb3ll@gmail.com>
2025-11-17 17:54:47 -0500

    Initial Commit

---
+++ MiniDVCapture/MiniDVCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniDVCaptureApp.swift
+//  MiniDVCapture
+//
+//  Created by JP on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct MiniDVCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
